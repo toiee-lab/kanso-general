606a65ab2235fd772bff523ed8807e97b4d07e88
attu19 <33148217+attu19@users.noreply.github.com>
2018-03-14 16:37:01 +0900

    404pageの改良
    404pageをKANSOテーマに適応させました。
    
    検索フォームをuikitを反映させました。
    
    404に画像を追加しました。

--- 404.php
+++ 404.php
@@ -9,52 +9,28 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'kanso-general' ); ?></h1>
-				</header><!-- .page-header -->
+	
+  <div  class="uk-container uk-container-small uk-background-default main-content" >
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'kanso-general' ); ?></p>
+	<h1>お探しのページは、見つかりませんでした</h1>
+	<h2 class="main-subtitle">Page is not found</h2>
+	
+	
+	
+<img src="<?php echo get_template_directory_uri(); ?>/images/notfound/notfound1.jpg">
+	
+	
+	
+	
+	<p>以下のフォームで検索することができます。</p>
+	
 
-					<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'kanso-general' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'kanso-general' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
+      <?php
+		get_search_form();
+	  ?>
+ 
+  </div><!-- .main-content -->
 <?php
+get_sidebar();
 get_footer();
