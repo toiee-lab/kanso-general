5382c6190fdbfd5426ab728754ea8c87e75fa746
T. Kameda <kame@hokuken.com>
2018-09-14 09:38:30 +0900

    サブタイトルをGutenbergに対応（のはず）
    meta_input を利用するように変更。クラスを使うようにした。でも、反映はされないな。

--- inc/register-lead-meta.php
+++ inc/register-lead-meta.php
@@ -1,60 +1,58 @@
 <?php
 	
-	add_action( 'add_meta_boxes', function (){
-		add_meta_box('kns_lead', 'サブタイトル', 'display_kns_lead_meta_box',
-			 array('page', 'post'), 'side', 'high'
-		);
-	} );
+	
+$kns_lead_obj = new Kns_Lead();
 
-	function display_kns_lead_meta_box(){
-			$id = get_the_ID();
+class Kns_Lead {
+	
+	public function __construct() {
+		global $pagenow;
+		if ( in_array( $pagenow,
+			array( 'post.php', 'post-new.php', 'page.php', 'page-new.php' ) ) ) {
+			add_action( 'admin_init', array( &$this, 'admin_init' ) );
 			
-			// embed
-			$kns_lead = get_post_meta($id, 'kns_lead', true);
-									
-			wp_nonce_field( 'kns_lead_meta_box', 'kns_lead_meta_box_nonce' );
-			echo <<<EOD
-<p><b>リード文(サブタイトル)</b>（任意）:<br>
-<input type="text" name="kns_lead" value="{$kns_lead}" style="width:100%"></p>
-EOD;
-			
+			add_action( 'save_post', array( &$this, 'save' ) );	
+		}
 	}
 	
+	function admin_init() {
+		add_action( 'add_meta_boxes', function (){
+			add_meta_box('kns_lead', 'サブタイトル', array( &$this, 'meta_box' ),
+				 array('page', 'post'), 'side', 'high'
+			);
+		} );
+	}
 	
-/*	add_action('edit_form_after_title', 'display_kns_lead_meta_box');
-	function display_kns_lead_meta_box( $post )
-	{
+	function meta_box() {
 		$id = get_the_ID();
 		
 		// embed
-		$kns_lead = get_post_meta($id, 'kns_lead', true);
+		$kns_lead = esc_attr( get_post_meta($id, 'kns_lead', true) );
 								
 		wp_nonce_field( 'kns_lead_meta_box', 'kns_lead_meta_box_nonce' );
-		echo <<<EOD
-<p><b>リード文(サブタイトル)</b>（任意）:<br>
-<input type="text" name="kns_lead" value="{$kns_lead}" style="width:100%"></p>
+			echo <<<EOD
+<p><b>サブタイトル</b>（任意）:<br>
+<input type="text" id="meta_kns_lead" name="meta_input[kns_lead]" value="{$kns_lead}" style="width:100%"></p>
 EOD;
 
 	}
-*/
-
-	add_action( 'save_post', 'save_kns_lead_meta_boxes' );	
-	function save_kns_lead_meta_boxes($post_id)
+	
+	function save($post_id)
 	{
 		
-        // Check if our nonce is set.
-        if ( ! isset( $_POST['kns_lead_meta_box_nonce'] ) ) {
-            return $post_id;
-        }
- 
-        $nonce = $_POST['kns_lead_meta_box_nonce'];
- 
-        // Verify that the nonce is valid.
-        if ( ! wp_verify_nonce( $nonce, 'kns_lead_meta_box' ) ) {
-            return $post_id;
-        }
+	    // Check if our nonce is set.
+		if ( ! isset( $_POST['kns_lead_meta_box_nonce'] ) ) {
+	        return $post_id;
+	    }
+	
+	    $nonce = $_POST['kns_lead_meta_box_nonce'];
+	
+	    // Verify that the nonce is valid.
+	    if ( ! wp_verify_nonce( $nonce, 'kns_lead_meta_box' ) ) {
+	        return $post_id;
+	    }
 		
-		$kns_lead = isset($_POST['kns_lead']) ? $_POST['kns_lead'] : null;		
+		$kns_lead = isset($_POST['meta_input']['kns_lead']) ? $_POST['meta_input']['kns_lead'] : null;
 		$before = get_post_meta($post_id, 'kns_lead', true);
 		
 		if($kns_lead)
@@ -74,3 +72,4 @@ EOD;
 			delete_post_meta($post_id, 'kns_lead', $before);
 		}
 	}
+}
