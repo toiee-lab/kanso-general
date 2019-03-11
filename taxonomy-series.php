38610e7fbcee8e3cbed8b1cac15e5ff078072aeb
T. Kameda <kame@hokuken.com>
2019-03-11 11:44:38 +0900

    podcastのエピソード一覧で、vimeoを直接表示
    vimeo のダウンロードリンクを使っている場合は、vimeoのプレイヤーを使う

--- taxonomy-series.php
+++ taxonomy-series.php
@@ -8,57 +8,59 @@
  */
 get_header(); ?>
 
-	<div class="uk-container uk-container-small uk-background-default" style="margin-top:3rem; margin-bottom: 3rem;">
+    <div class="uk-container uk-container-small uk-background-default" style="margin-top:3rem; margin-bottom: 3rem;">
 
 		<?php
 		if ( have_posts() ) : ?>
 
-			<header class="page-header uk-margin-medium-bottom">
-				<div uk-grid>
-				<?php
+            <header class="page-header uk-margin-medium-bottom">
+                <div uk-grid>
+					<?php
 					global $ss_podcasting;
 					global $wcr_content;
 					global $wcr_ssp;
-					
+
 					$series       = get_queried_object();
 					$series_id    = $series->term_id;
 					$series_url   = get_term_link( $series );
 					$series_image = get_option( 'ss_podcasting_data_image_' . $series_id, 'no-image' );
-					
+					$series_material = get_field( 'series_material', $series );
+
 					// 制限ありのpodcastなのかフラグ
 					$series_allow = true;
-					$restrict_pass  = get_option( 'ss_podcasting_wc_restrict_ssp_' . $series_id, false );  // デフォルトは fals				
-					if( $restrict_pass == 'restrict_enable' ) {
-						$ret = $wcr_ssp->get_access_and_product_url('', '', $series_id);
+
+					//新しい方の設定（ term の場合は、term object を渡す必要がある）
+					$wcr_content_ssp  = get_field( 'series_limit',  $series );
+
+					if( $wcr_content_ssp ) {
+						$ret = $wcr_ssp->get_access_and_product_url( '', '', $series_id );
 						$restrict_pass = $ret['access'];
 					}
-					
-					// - - - - - - 
-										
+
+					// - - - - - -
+
 					// podcast 登録案内ボタンの取得
 					$pcast_info = $wcr_ssp->add_wcr_ssp_shortcode(array(
-									'id'            => $series_id,
-									'template_name' => 'on_archive',
-									'redirect_url'  => $series_url
-									));
-				?>
-					<div class="uk-width-medium@m">
-						<img src="<?php echo $series_image; ?>">
-					</div>
-					<div class="uk-width-expand@m">
-				<?php
+						'id'            => $series_id,
+						'template_name' => 'on_archive',
+						'redirect_url'  => $series_url
+					));
+					?>
+                    <div class="uk-width-medium@m">
+                        <img src="<?php echo $series_image; ?>">
+                    </div>
+                    <div class="uk-width-expand@m series-header">
+						<?php
 
-					//the_archive_title( '<h1 class="uk-heading-primaryr">', '</h1>' );
-					$series_title = get_option( 'ss_podcasting_data_title_' . $series_id, '' );
-					echo '<h1 class="uk-h2">'.$series_title.'</h2>';
+						the_archive_title( '<h1 class="uk-heading-primaryr">', '</h1>' );
+						the_archive_description( '<p>', '</p>' );
+						echo $pcast_info;
+						echo $series_material;
 
-					the_archive_description( '<p>', '</p>' );
-					echo $pcast_info;
-					
-				?>
-					</div>
-				</div>
-			</header><!-- .page-header -->
+						?>
+                    </div>
+                </div>
+            </header><!-- .page-header -->
 
 			<?php
 			/* Start the Loop */
@@ -70,92 +72,98 @@ get_header(); ?>
 				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 				 */
 				//get_template_part( 'template-parts/content', get_post_format() );
-			?>
-			<article id="post-<?php the_ID(); ?>">
-				<?php
+				?>
+                <article id="post-<?php the_ID(); ?>">
+					<?php
 					if ( is_singular() ) :
 						the_title( '<h1 class="">', '</h1>' );
 					else :
 						the_title( '<h2 class="uk-h3">', '</h2>' );
 					endif;
-				?>
-				<div class="">
-					<?php
-						
-						
+					?>
+                    <div class="">
+						<?php
+
+
 						// get audio file
 						$audio_file = $ss_podcasting->get_enclosure( get_the_ID() );
-		                if ( get_option( 'permalink_structure' ) ) {
-		                    $enclosure = $ss_podcasting->get_episode_download_link( get_the_ID() );
-		                } else {
-		                    $enclosure = $audio_file;
-		                }		
-		                $enclosure = apply_filters( 'ssp_feed_item_enclosure', $enclosure, get_the_ID() );
-	                
+						if ( get_option( 'permalink_structure' ) ) {
+							$enclosure = $ss_podcasting->get_episode_download_link( get_the_ID() );
+						} else {
+							$enclosure = $audio_file;
+						}
+						$enclosure = apply_filters( 'ssp_feed_item_enclosure', $enclosure, get_the_ID() );
+
 						// get type (audio or video)
-		                $episode_type = $ss_podcasting->get_episode_type( get_the_ID() );
-		                if ( $episode_type == 'audio' ) {
-							$shortcode = '[audio src="'.$enclosure.'" /]';
+						$episode_type = $ss_podcasting->get_episode_type( get_the_ID() );
+						if ( $episode_type == 'audio' ) {
+							$html_player = do_shortcode( '[audio src="'.$enclosure.'" /]' );
 						}
 						else {
-							$shortcode = '[video src="'.$audio_file.'" /]';
+							if( preg_match('|https://player.vimeo.com/external/([0-9]+)|', $audio_file, $matches) ) {
+								$vid = $matches[1];
+								//$html_player = '<iframe src="https://player.vimeo.com/video/'.$vid.'" width="640" height="480" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+								$html_player = '<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/'.$vid.'?title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>';
+							}
+							else{
+								$html_player = do_shortcode( '[video src="'.$audio_file.'" /]' );
+							}
 						}
-						
+
 						$episode_restrict = get_post_meta( get_the_ID(), 'wcr_ssp_episode_restrict', 'disable' );
 						if( $restrict_pass || ($episode_restrict != 'enable' ) ){
 							// 表示
-						?>
+							?>
 
-					<div class="uk-margin-medium-top uk-margin-small-bottom">
-		                <?php echo do_shortcode( $shortcode ); ?>
-					</div>					
-						
-						<?php	
+                            <div class="uk-margin-medium-top uk-margin-small-bottom">
+								<?php echo $html_player; ?>
+                            </div>
+
+							<?php
 						}
 						else{
 							// 非表示にする
-			                if ( $episode_type == 'audio' ) {
+							if ( $episode_type == 'audio' ) {
 								echo $wcr_ssp->add_wcr_ssp_shortcode(array(
 									'id'            => $series_id,
 									'template_name' => 'on_episode_audio',
 									'label_trial'   => '',
 									'label_offer_trial' => '',
 									'redirect_url'  => $series_url
-									));
+								));
 							}
 							else {
 								echo $wcr_ssp->add_wcr_ssp_shortcode(array(
 									'id'            => $series_id,
 									'template_name' => 'on_episode_video',
 									'label_trial'   => '',
-									'label_offer_trial' => '',									
+									'label_offer_trial' => '',
 									'redirect_url'  => $series_url
-									));
+								));
 							}
 						}
-						
-					?>
-					<div>
-						<p><a href="#" uk-toggle="target: #desc<?php the_id(  );?>">説明を表示する</a></p>
-						<div id="desc<?php the_id(  );?>" hidden>
-					<?php
-						the_content( );
-					?>
-						</div>
-						<div class="entry-meta uk-text-right uk-margin">
-							<?php kanso_general_posted_on(); ?>
-						</div><!-- .entry-meta -->					
 
-					</div>
-				</div><!-- .entry-content -->
-			
-				<footer class="entry-footer">
-					<?php kanso_general_entry_footer(); ?>
-				</footer><!-- .entry-footer -->
-			</article><!-- #post-<?php the_ID(); ?> -->
-			<hr class="uk-margin-medium-top uk-margin-medium-bottom">
+						?>
+                        <div>
+                            <div id="desc<?php the_id(  );?>" >
+								<?php
+								the_content( );
+								?>
+                            </div>
+                            <div class="entry-meta uk-text-right uk-margin">
+								<?php kanso_general_posted_on(); ?>
+                            </div><!-- .entry-meta -->
 
-<?php	
+                        </div>
+                    </div><!-- .entry-content -->
+
+                    <footer class="entry-footer">
+						<?php kanso_general_entry_footer(); ?>
+                    </footer><!-- .entry-footer -->
+                </article><!-- #post-<?php the_ID(); ?> -->
+                <hr class="uk-margin-medium-top uk-margin-medium-bottom">
+
+			<?php
 			endwhile;
 
 			//the_posts_navigation();<ul class='page-numbers'>
@@ -167,7 +175,7 @@ get_header(); ?>
 			) );
 			$pagenation = str_replace("<ul class='page-numbers'>", "<ul class='uk-pagination' uk-margin>", $pagenation);
 			echo $pagenation;
-			
+
 
 		else :
 
@@ -175,7 +183,7 @@ get_header(); ?>
 
 		endif; ?>
 
-	</div><!-- .main-content -->
+    </div><!-- .main-content -->
 
 <?php
 get_sidebar();
