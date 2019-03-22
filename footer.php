f99f2c874cecf30bb67b9ca43b38081817369aec
T. Kameda <kame@hokuken.com>
2019-03-22 09:27:50 +0900

    v1.0.1
    - 白テーマに import をつけ忘れた
    - footer や sidebar の修正
    - shortcode の warning を修正
    
    （コミットし忘れていた・・・。すみません）

--- footer.php
+++ footer.php
@@ -11,6 +11,22 @@
 
 ?>
 
+<footer id="footer" uk-height-viewport="expand: true">
+	<div  class="uk-grid-collapse uk-child-width-expand@s uk-text-left" uk-grid>
+
+		<div>
+			<?php dynamic_sidebar( 'footer-left' ); ?>
+		</div>
+		<div>
+			<?php dynamic_sidebar( 'footer-center' ); ?>
+		</div>
+		<div>
+			<?php dynamic_sidebar( 'footer-right' ); ?>
+		</div>
+
+	</div>
+	<p id="footer-copyright">Copyright &copy; <span class="ownername"><?php echo esc_html( get_option( 'kanso_general_options_ownername' ) ); ?></span>, All rights reserved.</p>
+</footer><!-- #footer -->
 
 <?php wp_footer(); ?>
 
